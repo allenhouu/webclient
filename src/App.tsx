35ab39c8204441b21402775ed0b7fc8827af59c8
allenhouu <126ahou@frhsd.com>
2026-01-08 08:33:17 -0500

    commit + push

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+    useEffect(() => {
+        const API_URL = 'http:localhost:3000';
+
+        fetch(API_URL)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+        let data;
+        let index;
+        fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }) .then((res) => {
+            if(!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+
+            })
+
+        fetch(API_URL, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({data, index}),
+        }).then((res) => {
+            if(!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+        })
+    }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
+
+
 
 export default App
